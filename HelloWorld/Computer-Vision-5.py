7dee32e965c1db829bcbd2e82f89f0013347d838
Mattheus Medina Ayala <mataus94@outlook.com>
2022-12-01 01:28:04 -0300

    Update Computer-Vision-5.py

--- HelloWorld/Computer-Vision-5.py
+++ HelloWorld/Computer-Vision-5.py
@@ -19,20 +19,27 @@ pre_trained_model = InceptionV3(input_shape=(150, 150, 3),
 # carrega o arquivo de pesos no modelo pré-treinado
 pre_trained_model.load_weights(weights_file)
 
+# para cada camada no modelo pré-treinado
 for layer in pre_trained_model.layers:
+ # o parametro "trainable" recebe falso, significando que a camada não será mais treinada (eu acho)
  layer.trainable = False
+# salva a camada 'mixed7' do modelo pré-treinado na variável "last_layer"
 last_layer = pre_trained_model.get_layer('mixed7')
+# printa o formato da ultima camada(last_layer) do modelo
 print('last layer output shape: ', last_layer.output_shape)
+# salva o formato de saida da ultima camada na variável "last_output"
 last_output = last_layer.output
 
-for layer in pre_trained_model.layers:
- layer.trainable = False
-last_layer = pre_trained_model.get_layer('mixed7')
-print('last layer output shape: ', last_layer.output_shape)
-last_output = last_layer.output
+# Salva em x uma camada planificada(de 1 dimensão) originada de "last_output"
+x = layers.Flatten()(last_output)
+# Salva em x outra camada, com 1024 neurônios e ativação relu que se soma a camada x anterior
+x = layers.Dense(1024, activation='relu')(x)
+# Salva em x uma última camada, sigmoid, que gera o output.
+x = layers.Dense(1, activation='sigmoid')(x)
 
-for layer in pre_trained_model.layers:
- layer.trainable = False
-last_layer = pre_trained_model.get_layer('mixed7')
-print('last layer output shape: ', last_layer.output_shape)
-last_output = last_layer.output
+# define o modelo de treino com a camada de input do modelo pré-treinado do google e as camadas salvas em x.
+model = keras.Model(pre_trained_model.input, x)
+# compila o modelo com o otimizador RMSprop(Root Mean Square Propagation), algoritimo de perda de entropia cruzada binária e métrica acc
+model.compile(optimizer=optimizers.RMSprop(learning_rate=0.0001),
+ loss='binary_crossentropy',
+ metrics=['acc'])
